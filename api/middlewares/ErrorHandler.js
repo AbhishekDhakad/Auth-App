b5a3781384133e8e3485bbcda82d9084775302d4
AbhishekDhakad <abhishek.dhakad121@gmail.com>
2023-11-07 00:24:58 +0530

    Create Signup page

--- api/middlewares/ErrorHandler.js
+++ api/middlewares/ErrorHandler.js
@@ -1,7 +1,6 @@
 const ErrorHandler = (err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Something went wrong";
-    console.log("hello error")
     res.status(statusCode).json({
         success: false,
         status: statusCode,
